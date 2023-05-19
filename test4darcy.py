cf2239e3daeade1865bc08268272ce4d97a3b63d
Darcymer <91292053+Darcymer@users.noreply.github.com>
2023-05-18 21:23:22 -0400

    test file

---
+++ test4darcy.py
@@ -0,0 +1,13 @@
+#Simple code to compare the two homograpghs
+path1 = input("What is your first file path?")
+path2 = input("What is your second file path?")
+
+if path1 == path2:
+    print("The sets are homographs")
+else:
+    print("Files are diffrent")
+
+print("Have a good day")
+
+
+#this is a basic idea of what we are doiung but WAY longer
